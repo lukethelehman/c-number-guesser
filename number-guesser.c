953dfbd230b0284a9b693e55fd21a6cfb1166bdf
lukethelehman <lukelehman05@gmail.com>
2025-01-23 03:22:40 +0000

    Made functional number guesser minus an end message about turns

--- number-guesser.c
+++ number-guesser.c
@@ -3,10 +3,53 @@
 #include <time.h>
 
 int main(){
-	printf("hello world \n");
 	srand(time(NULL));
+	char name[20];
+	int guess;
+	int turn = 0;
+	int keepGoing = 1;
+	int correct = ((rand() % 100) + 1);
+
+	printf("Hello! What is your name? \n");
+	scanf("%s", name);
+	printf("Hi, %s, let's play a game! \n", name);
+
+	while (keepGoing == 1 ) {
+		turn ++;
+		printf("Guess a number between 1-100! \n");
+		scanf("%d", &guess);
+		
+		if (guess < correct) {
+			printf("Too low!\n");
+		}
+		if (guess > correct) {
+			printf("Too high!\n");
+		}
+		if (guess == correct) {
+			printf("Right on!\n");
+			keepGoing = 0;
+		}
+
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+	/*
 	for (int i = 0; i < 10; i++){
 	int myNumber = (rand() % 6) + 1;
 	printf("%d \n", myNumber);}
 	return 0;
+	*/
+
 } //end main
